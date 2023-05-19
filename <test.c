48b6c2fbe22bde49e8774fbef6493b0fa4a2fcec
DavAnzal3 <133888343+DavAnzal3@users.noreply.github.com>
2023-05-19 21:39:34 +0000

    new file whit a part of semple code in C

---
+++ <test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello\n");
+
+}
+
+/* Nuova libreria per funzioni non disponibili in C*/
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main() {
+    risposta = get_string("Whats");
+    printf("Hello\n");
+
+}
+
+
