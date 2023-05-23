b3ac3282b5a3659c1523a633528c63603a5727bc
DavAnzal3 <133888343+DavAnzal3@users.noreply.github.com>
2023-05-23 14:20:57 +0000

    update

---
+++ Function.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+void meow(int n);   /*no input (void)*/
+
+int main() {
+
+    meow(3);
+   
+
+}
+
+void meow(int n){
+    
+    for (int i =0; i < n; i++)
+    {
+        printf("Meow\n");
+    }
+    
+}
