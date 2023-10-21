5128c23028e8f1a1db0ba92ad66a2e5681faafdf
DavAnzal3 <133888343+DavAnzal3@users.noreply.github.com>
2023-10-21 22:56:01 +0200

    Update Hello.py

--- Python/Hello.py
+++ Python/Hello.py
@@ -1,1 +1,19 @@
-print("Hello, world") 
+def trova_regione(targa):
+    codice_provincia = targa[:2]
+    
+    # Esempio di mappatura dei codici di provincia alle regioni
+    regioni = {
+        "RM": "Lazio",
+        "MI": "Lombardia",
+        # Altri codici qui...
+    }
+    
+    if codice_provincia in regioni:
+        return regioni[codice_provincia]
+    else:
+        return "Regione non riconosciuta"
+
+# Esempio di utilizzo:
+targa = "RM123AB"
+regione = trova_regione(targa)
+print(f"La targa {targa} è stata immatricolata nella regione: {regione}")
